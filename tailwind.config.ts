e3d3469b5c7bee04fa9151556d0f756fa6e4dada
yesphotography-Rgb <afndynaa.ahmed@gmail.com>
2026-01-08 07:16:44 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,5 @@
+import { type Config } from "tailwindcss";
+
+export const content = ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"];
+export const theme = { extend: {} };
+export const plugins = [];
